0e1c3344259fa5de754ec99c4a6a2626febfd4e4
JulianDev <julianrosemberg2@gmail.com>
2026-01-15 17:51:09 -0300

    Hago la tarea para poder seguir con el curso.

--- Basic/05-operators-exercises-to-do.js
+++ Basic/05-operators-exercises-to-do.js
@@ -1,47 +1,54 @@
-//strings
+// 01-operators-exercises.js para cada operación
 
-//concatenacion
-let myName = "julian"
-let greeting = "Hola, " + myName + "!"
+// 1. Crea una variable para cada operación aritmética
+let n1 = 3 ,n2 =5
 
-console.log(greeting)
-console.log(typeof greeting)
+console.log(n1 + n2)
+console.log(n1 - n2)
+console.log(n1 * n2)
+console.log(n1 / n2)
+console.log(n1 % n2)
+console.log(n1 ** n2)
+n1++ //tambien se puede hacer como la siguente linea
+n2 -= 2
+console.log(`El numero uno es: ${n1} y el numero dos es: ${n2}.`)
 
-console.log("--------------------------")
 
-//longitud
-console.log(greeting.length)
 
-console.log("--------------------------")
+// 2. Crea una variable para cada tipo de operación de asignación, 
+//    que haga uso de las variables utilizadas para las operaciones aritméticas
+let variableAsignacion = 2
 
-//acceso a caracteres
-console.log(greeting[0])
-console.log(greeting[12])
 
-console.log("--------------------------")
+console.log("Antes: " + variableAsignacion)
 
-//metodos comunes
-console.log(greeting.toUpperCase())
-console.log(greeting.toLowerCase())
-console.log(greeting.indexOf("julian"))
-console.log(greeting.indexOf("Hola"))
-console.log(greeting.indexOf("Rosemberg"))
-console.log(greeting.includes("julian"))
-console.log(greeting.includes("Hola"))
-console.log(greeting.includes("Rosemberg"))
-console.log(greeting.slice(0, 10))
-console.log(greeting.replace("julian", "rosemberg"))
+variableAsignacion += 1
+variableAsignacion -= 1
+variableAsignacion *= 100
+variableAsignacion /= 10
+variableAsignacion %= 3
+variableAsignacion **= 2
+console.log("Despues: " + variableAsignacion)
 
-console.log("--------------------------")
+// 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
 
-//templates literals (plantillas literales)
-let message = `hola, este
-es mi
-curso de 
-JavaScript`
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
 
-console.log(message)
+// 5. Utiliza el operador lógico and
 
-let email = "julianrosemberg2@gmail.com"
+// 6. Utiliza el operador lógico or
 
-console.log(`Hola, ${myName}! Tu email es ${email}`)
+
+// 7. Combina ambos operadores lógicos
+
+// 8. Añade alguna negación
+console.log(n1, n2)
+console.log(n1 > n2 && n1!= 0)
+
+// 9. Utiliza el operador ternario
+const isflag = true
+
+isflag ? console.log("Verdadero!") : console.log("Falso!")
+// 10. Combina operadores aritméticos, de comparación y lógicos
+console.log(n1++ * n2 > n2-- / 2 && 2 == 2)
+console.log(n1++ * n2 > n2-- / 2 && 1 == 2)
--- Basic/07-strings-exercises-to-do.js
+++ Basic/07-strings-exercises-to-do.js
@@ -1,21 +1,38 @@
 // strings-exercises.js
 
 // 1. Concatena dos cadenas de texto
+let miNombre = "Julian"
+console.log(`Hola, mi nombre es ${miNombre}!`)
 
 // 2. Muestra la longitud de una cadena de texto
+console.log(`La lungitud de mi nombre es: ${miNombre.length}`)
 
 // 3. Muestra el primer y último carácter de un string
+console.log(`El primer caracter de mi nombre es: ${miNombre[0]} y el ultimo caracter de mi nombre es: ${miNombre[5]}`)
 
 // 4. Convierte a mayúsculas y minúsculas un string
+console.log(miNombre.toLocaleUpperCase()) //como es una funcion o metodo hay que agregar los ()
 
 // 5. Crea una cadena de texto en varias líneas
+console.log(`Hola amigos.
+como estan?`)
 
 // 6. Interpola el valor de una variable en un string
+//hecho
 
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
+let text1 = "Hola este es un ejercicio de JS hecho por " + miNombre
+console.log(text1.replace(/\s+/g, "-"))
+//\s+ → busca uno o más espacios en blanco (incluye espacios, tabulaciones y saltos de línea).
+//g → bandera global, asegura que se reemplacen todos los espacios en el string.
+//"-" → es el carácter que usaremos para reemplazar.
+
 
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+console.log(text1.includes("Julian"))
 
 // 9. Comprueba si dos strings son iguales
+console.log(text1.includes(miNombre))
 
 // 10. Comprueba si dos strings tienen la misma longitud
+console.log(text1.length == miNombre.length)
